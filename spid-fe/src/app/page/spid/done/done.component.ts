efda936c53b0eef047590925bad381a4ca54c80a
Giacomo-it <giacomomsidp@gmail.com>
2024-12-28 11:15:26 +0100

    feat: add login component

---
+++ spid-fe/src/app/page/spid/done/done.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-done',
+  imports: [],
+  templateUrl: './done.component.html',
+  styleUrl: './done.component.css'
+})
+export class DoneComponent {
+
+}
---
+++ spid-fe/src/app/page/spid/spid.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SpidRoutingModule } from './spid-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SpidRoutingModule
+  ]
+})
+export class SpidModule { }
