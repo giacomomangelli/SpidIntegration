0cf32f43d0bc9e78d2c87a0c9ff00602d5ad1b0e
Giacomo-it <giacomomsidp@gmail.com>
2024-12-31 00:32:20 +0100

    wip: integrate cie button

--- spid-fe/src/app/service/metadata.service.ts
+++ spid-fe/src/app/service/metadata.service.ts
@@ -2,13 +2,14 @@ import {Injectable} from '@angular/core';
 import {Metadata} from '../model/metadata';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MetadataService {
 
-  BASE_URL = 'http://localhost:3000/metadata/';
+  BASE_URL = environment.url + 'metadata/';
 
   constructor(private readonly http: HttpClient) {
   }
--- spid-fe/src/app/service/spid.service.ts
+++ spid-fe/src/app/service/spid.service.ts
@@ -1,19 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpidService {
 
-  BASE_URL = 'http://localhost:3000/spid/';
+  BASE_URL = environment.url + 'spid/';
 
-  constructor(private readonly http: HttpClient) { }
-
+  constructor(private readonly http: HttpClient) {
+  }
 
   public getAuthRequest(clientId: string, idp: string): Observable<any> {
     console.log('Get Auth Request');
     return this.http.get(`${this.BASE_URL}?client_id=${clientId}&idp=${idp}`);
+  }
+
+  public redirectToIdp(ssoRequest: string) {
+    return
   }
 }
