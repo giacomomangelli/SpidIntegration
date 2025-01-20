3b3617a712594aef2d5e71bed415eca071423ff6
Giacomo-it <giacomomsidp@gmail.com>
2025-01-20 01:32:18 +0100

    refactor: remove redundant library

--- spid-fe/src/app/assets/spid/js/config-dev.js
+++
@@ -1,6 +0,0 @@
-/*
- * Test url locale valida quando la index è avviata con grunt-serve
- */
-SPID.assetsBaseUrl = '';
-SPID.stylesheetUrl = 'dist/spid-button.min.css';
-
--- spid-fe/src/app/assets/spid/js/config.js
+++
@@ -1,5 +0,0 @@
-/**
- * Indirizzi di produzione delle risorse (attualmente test), inserire indirizzi reali di CDN
- */
-SPID.assetsBaseUrl = 'https://italia.github.io/spid-smart-button/';
-SPID.stylesheetUrl = 'https://italia.github.io/spid-smart-button/spid-button.min.css';
